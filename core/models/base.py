58188a3412dd998b958adead62560b535db730e3
aybuekeulusarslan <aybuke.ulusarslan@tum.de>
2025-06-18 11:59:09 +0200

    Implemented the Topotecan model

--- core/models/base.py
+++ core/models/base.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
-from typing import Optional, Callable
+from typing import Optional, Callable, Union
 
+import jax
 import jax.numpy as jnp
 from jax import Array
 
@@ -19,18 +20,28 @@ class DynamicalSystem(ABC):
         - g: X x \Theta \mapsto Y
     """
     
-    def __init__(self, n: int, m: int, p: Optional[int] = None, d: Optional[int] = None):
+    def __init__(self, 
+                 n: int, # State dim.
+                 m: int, # Input dim.
+                 p: Optional[int] = None, #  Output dim.
+                 params: Optional[Array] = None,
+                 integrator: Optional[Callable]=None):
         assert isinstance(n, int) and n > 0
-        self.n = n
-        if d is not None:
-            assert isinstance(d, int) and d > 0
-        self.d = d
+        self.n = n 
+        self.m = m 
+        assert self.p <= self.n 
         self.p = p or n
-        assert self.p <= self.n        
+        if params:
+            self.d = max(params.shape)
+            self.params = params 
+        else:
+            self.d = 0
+        # Custom (y0, t, \theta) -> y(t) or RK of order 4
+        self.integrator = integrator or self._rk4_step 
 
     
     @abstractmethod
-    def f(self, state:Array, input: Array, t:float) -> Array:
+    def f(self, state:Array, input: Optional[Array], t:float) -> Array:
         """
         System dynamics. Computes the derivative of the state vector at time t.
         
@@ -64,3 +75,52 @@ class DynamicalSystem(ABC):
             \dot{x}(t) \in \mathbb{R}^{n \times 1}
         """
         
+    def _rk4_step(
+        self, 
+        f: Callable, 
+        x: Array,
+        u: Optional[Array], 
+        t: float, 
+        dt: float,
+    ):
+        k1 = f(x, u, t)
+        k2 = f(x +0.5*dt*k1, u, t + 0.5*dt)
+        k3 = f(x + 0.5*dt*k2, u, t + 0.5*dt)
+        k4 = f(x+ dt*k3, u, t + dt)
+        
+        return x + (dt/6)*(k1 + 2*k2 + 2*k3 + k4)
+    
+    def simulate(self, x0: Array, time_steps: Array, controls: Optional[Array]) -> Array:
+        T = time_steps.shape[0]
+        if controls is None:
+            controls = jnp.zeros((T, self.m))
+        else:
+            assert T == controls.shape[0], f"Length of the control sequence must match that of time steps!"
+            assert self.m == controls.shape[1], f"Control inputs must have the dimension m = {self.m}! Provided sequence: ({controls.shape[0]}, {controls.shape[1]})"
+        
+        def step(carry, idx):
+            x_prev, t_prev = carry
+            t_curr = time_steps[idx]
+            dt = t_curr - t_prev
+            u_curr = controls[idx]
+            x_next = self.integrator(self.f, x_prev, u_curr, self.params, t_prev, dt)
+            return (x_next, t_curr), x_next
+        
+        _, xs = jax.lax.scan(step, (x0, time_steps[0]), jnp.arange(1, T))
+        return jnp.vstack((x0[None, :], xs))
+    
+    def observe(self, states: Array, times : Array) -> Array:
+        vg = jax.vmap(lambda x, t: self.g(x, self.params, t), in_axes=(0,0))
+        return vg(states, times)
+    
+    def perturb_process(self, states: Array) -> Array:
+        """
+        Override to inject process noise.
+        """
+        return states
+    
+    def perturb_measurements(self, outputs: Array) -> Array:
+        """
+        Override to inject measurement noise.
+        """
+        return outputs
